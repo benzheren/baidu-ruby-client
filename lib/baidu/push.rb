94ccb51a3f693cd67da4935608d1b74f01db0cfb
Ben Hu <benzheren@gmail.com>
2013-08-19 19:50:43 +0800

    set default value for push parameters

--- lib/baidu/push.rb
+++ lib/baidu/push.rb
@@ -2,10 +2,13 @@
 module Baidu
   class Push
     def self.push_msg(data)
-      body = data.merge({ method: 'push_msg', message_type: 1 })
+      body = { method: 'push_msg',
+               message_type: 1,
+               push_type: 3 }
+      
       uri = Protocol.push_uri
 
-      response = Baidu.client.post(:channel, uri, body)
+      response = Baidu.client.post(:channel, uri, body.merge(data))
     end
   end
 end
